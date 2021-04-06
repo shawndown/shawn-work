db7cd2a91830b2de60240b04e841f86dffbbbdac
shawn <532959030@qq.com>
2021-04-06 13:47:08 +0930

    - add map funtcion - add drawer - add bottom navigation bar

--- src/unisa_map/lib/maps.dart
+++ src/unisa_map/lib/maps.dart
@@ -1,66 +1,194 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import './settings.dart';
+import './main.dart';
 
 const mainColor = const Color(0xFF4166F6);
 
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('${this} hashCode=${this.hashCode}');
+    // print('${this} hashCode=${this.hashCode}');
+
     return Scaffold(
+      endDrawerEnableOpenDragGesture: false,
       appBar: AppBar(
         title: Text('UNISA MAP'),
         backgroundColor: mainColor,
-        leading: Icon(Icons.menu),
-        actions: [
-          Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: Icon(
-                Icons.settings,
-                size: 25,
-              )),
+        leading: Builder(
+          builder: (context) => IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () => Scaffold.of(context).openDrawer()),
+        ),
+        actions: <Widget>[
+          TextButton.icon(
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SettingScreen(),
+                ),
+              ),
+            },
+            icon: Icon(
+              Icons.settings,
+              size: 25,
+              color: Colors.white,
+            ),
+            label: Text(''),
+          ),
         ],
       ),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width / 2,
-          height: MediaQuery.of(context).size.height / 2,
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("LOGOUT"),
-                  style: ElevatedButton.styleFrom(
-                      primary: mainColor, padding: EdgeInsets.all(15)),
-                ),
+      body: Container(
+        // width: MediaQuery.of(context).size.width / 2,
+        // height: MediaQuery.of(context).size.height / 2,
+        child: UnisaMap(),
+      ),
+      bottomNavigationBar: BottomWidget(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('UniSA MAP'),
+              decoration: BoxDecoration(
+                color: mainColor,
               ),
-              Container(
-                margin: EdgeInsets.only(left: 0, bottom: 0, top: 45, right: 0),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SettingScreen(),
-                      ),
-                    ),
-                  },
-                  child: Text("SETTINGS"),
-                  style: ElevatedButton.styleFrom(
-                      primary: mainColor, padding: EdgeInsets.all(15)),
-                ),
-              ),
-            ],
-          ),
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingScreen(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('logout'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginStatefulWidget(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class UnisaMap extends StatefulWidget {
+  @override
+  State<UnisaMap> createState() => UnisaMapState();
+}
+
+class UnisaMapState extends State<UnisaMap> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(-34.80903299847858, 138.61987035709035),
+    zoom: 16,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    );
+
+    // return new Scaffold(
+    //   body: GoogleMap(
+    //     mapType: MapType.hybrid,
+    //     initialCameraPosition: _kGooglePlex,
+    //     onMapCreated: (GoogleMapController controller) {
+    //       _controller.complete(controller);
+    //     },
+    //   ),
+    //   floatingActionButton: FloatingActionButton.extended(
+    //       onPressed: _goToTheLake,
+    //       label: Text('To the lake!'),
+    //       icon: Icon(Icons.directions_boat)),
+    // );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class BottomWidget extends StatefulWidget {
+  const BottomWidget({Key? key}) : super(key: key);
+
+  @override
+  _BottomWidgetState createState() => _BottomWidgetState();
+}
+
+class _BottomWidgetState extends State<BottomWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: UNISA MAP',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: TIMETABLE',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: NAVIGATION',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map_outlined),
+          label: 'UNISA MAP',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.schedule_outlined),
+          label: 'TIMETABLE',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.pin_drop_outlined),
+          label: 'NAVIGATION',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: mainColor,
+      onTap: _onItemTapped,
     );
   }
 }
